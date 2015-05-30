029f7194b8b60fa759dff7ca1271a4399351a13b
snowhoro <snowhoro@users.noreply.github.com>
2015-05-30 16:51:50 -0300

    Sayonara

--- Inaba-Erio/MyEngine/Renderer/RenderTypes.cpp
+++
@@ -1,5 +0,0 @@
-#include "RenderTypes.h"
-#include <d3dx9.h>
-
-const DWORD Inaba::ColorVertexType = (D3DFVF_XYZ | D3DFVF_DIFFUSE);
-const int Inaba::TextureCoordVertexType = (D3DFVF_XYZ | D3DFVF_TEX1);
